81940fa403cba8d4d4df656f753afbf59f7d48c6
Adam <adahy.to@gmail.com>
2019-09-01 21:26:38 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // mongoDB database URL
 module.exports={
-  mongoDbUrl:'mongodb://adam:Fedora16#@ds215338.mlab.com:15338/recipes'
+  mongoDbUrl:'mongodb://adam:Fedora16Fedora@ds215338.mlab.com:15338/recipes'
 };
